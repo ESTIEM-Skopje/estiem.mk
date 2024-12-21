19803b4c08021b15abb5b50c2ecad534a68f6a85
Petar Hristovski <petarhristovski23@gmail.com>
2024-12-21 15:47:16 +0100

    adding animations

--- src/sections/whyJoinUsComponent.tsx
+++ src/sections/whyJoinUsComponent.tsx
@@ -1,51 +1,49 @@
+"use client";
 import Image from "next/image";
 import JoinUsComponent from "../components/JoinUsComponent";
 import SubheadingComponent from "@/components/SubheadingComponent";
 
-import Img1 from "@/images/logos_icons/Line.svg";
+import Img1 from "@/images/logos_icons/red_line_full.svg";
 import Img2 from "@/images/logos_icons/Logo.svg";
 import Img3 from "@/images/logos_icons/Logo.svg";
 import Img4 from "@/images/logos_icons/Logo.svg";
 import Img5 from "@/images/background.jpg";
+import { RevealList } from "next-reveal";
 
 const whyJoinUsComponent = () => {
   return (
-    <div className="lg:flex grid justify-center gap-20">
-      <div className="m-2 justify-items-center">
-        <SubheadingComponent text="Why Join Us?" img={Img1} />
-        <JoinUsComponent
-          img={Img2}
-          heading={"Lorem Ipsum!"}
-          text={
-            "Lorem Ipsum!"
-          }
-        />
-        <JoinUsComponent
-          img={Img3}
-          heading={"Lorem Ipsum!"}
-          text={
-            "Lorem Ipsum!"
-          }
-        />
-        <JoinUsComponent
-          img={Img4}
-          heading={"Lorem Ipsum!"}
-          text={
-            "Lorem Ipsum!"
-          }
-        />
+    <RevealList interval={100} delay={500}>
+      <div className="lg:flex grid justify-center gap-20">
+        <div className="m-2 justify-items-center">
+          <SubheadingComponent text="Why Join Us?" img={Img1} />
+          <JoinUsComponent
+            img={Img2}
+            heading={"Lorem Ipsum!"}
+            text={"Lorem Ipsum!"}
+          />
+          <JoinUsComponent
+            img={Img3}
+            heading={"Lorem Ipsum!"}
+            text={"Lorem Ipsum!"}
+          />
+          <JoinUsComponent
+            img={Img4}
+            heading={"Lorem Ipsum!"}
+            text={"Lorem Ipsum!"}
+          />
+        </div>
+        <div className="md:w-1/2 p-4 flex justify-center items-center order-1 md:order-2">
+          <Image
+            src={Img5}
+            alt="Feature Image"
+            width={600}
+            height={600}
+            style={{ border: "5px solid #1b5c04", borderRadius: "5%" }}
+            className="zoom"
+          />
+        </div>
       </div>
-      <div className="md:w-1/2 p-4 flex justify-center items-center order-1 md:order-2">
-        <Image
-          src={Img5}
-          alt="Feature Image"
-          width={600}
-          height={600}
-          style={{ border: "5px solid #1b5c04", borderRadius: "5%" }}
-          className="zoom"
-        />
-      </div>
-    </div>
+    </RevealList>
   );
 };
 export default whyJoinUsComponent;
